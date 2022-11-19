8483ef860c747c2efed40f2bc825c13d68b8dd18
size12 <astarchik1@yandex.ru>
2022-11-19 11:43:50 +0300

    added some lines of code

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{Addr: "127.0.0.1:8080", Handler: mux}
+	err := server.ListenAndServe()
+	log.Fatal(err)
+}
