5232b7aa5d7c11b83f9d850434e49ade7912405a
size12 <astarchik1@yandex.ru>
2022-11-19 20:00:56 +0300

    add test

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -8,12 +8,14 @@ import (
 )
 
 func TestURLHandler(t *testing.T) {
-	var code int = 201
+	code := 201
 	request, _ := http.NewRequest(http.MethodPost, "/", strings.NewReader("https://ya.ru"))
 	w := httptest.NewRecorder()
 	h := http.HandlerFunc(URLHandler)
 	h.ServeHTTP(w, request)
 	res := w.Result()
+
+	defer res.Body.Close()
 	if res.StatusCode != code {
 		t.Error("Wrong code")
 	}
