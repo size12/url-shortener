a4980545a199fa7aa6e24bd6538f69ec1dcdff25
size12 <astarchik1@yandex.ru>
2022-12-11 12:13:28 +0300

    added flags

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,15 +1,28 @@
 package config
 
-import "github.com/caarlos0/env/v6"
+import (
+	"flag"
+	"github.com/caarlos0/env/v6"
+)
 
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseURL       string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
-	StoragePath   string `env:"FILE_STORAGE_PATH"`
+	StoragePath   string `env:"FILE_STORAGE_PATH" envDefault:""`
 }
 
 func GetConfig() Config {
 	var cfg Config
 	env.Parse(&cfg)
+	if flag.Lookup("a") != nil {
+		flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "Server address")
+	}
+	if flag.Lookup("b") != nil {
+		flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "Base URL")
+	}
+	if flag.Lookup("c") != nil {
+		flag.StringVar(&cfg.StoragePath, "f", cfg.StoragePath, "Storage Path")
+	}
+	flag.Parse()
 	return cfg
 }
